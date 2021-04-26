8d33f81098947b3faae08991725f049fed02fb82
Luiz Felippo <coelho.luizfelippo@gmail.com>
2021-04-26 15:14:48 -0300

    add the solution of the sudoku

--- Solver.py
+++ Solver.py
@@ -1,3 +1,4 @@
+import numpy as np
 board = [5, 3, 0, 0, 7, 0, 0, 0, 0], \
         [6, 0, 0, 1, 9, 5, 0, 0, 0], \
         [0, 9, 8, 0, 0, 0, 0, 6, 0], \
@@ -17,10 +18,29 @@ def possible(row, col, n):
     for i in range(0, 9):
         if board[i][col] == n:
             return False
-    row0 = (row//3)*3
-    col0 = (col//3)*3
+    row0 = (row // 3) * 3
+    col0 = (col // 3) * 3
 
     for i in range(0, 3):
         for j in range(0, 3):
-            if board[row0+i][col0+j] == n:
+            if board[row0 + i][col0 + j] == n:
                 return False
+
+    return True
+
+
+def solve():
+    global board
+    for x in range(9):
+        for y in range(9):
+            if board[x][y] == 0:
+                for i in range(1, 10):
+                    if possible(x, y, i):
+                        board[x][y] = i
+                        solve()
+                        board[x][y] = 0
+                return
+    print(np.matrix(board))
+
+
+solve()
